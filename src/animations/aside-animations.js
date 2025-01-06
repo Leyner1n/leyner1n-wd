91e3a72ba9d2ceb74c7d7d6738378b0bf77c707f
Mikail Chughtai <mokhsinovich@mail.ru>
2025-01-07 01:32:15 +0300

    refactor

--- src/animations/aside-animations.js
+++ src/animations/aside-animations.js
@@ -1,28 +1,30 @@
-const homeBtn = document.getElementById('home-btn');
-const createBtn = document.getElementById('create-card');
-const notifnotificationsBtn = document.getElementById('notifications-btn');
-const messageBtn = document.getElementById('message-btn');
-const articleFormCloseBtn = document.getElementById('close-btn');
+const sidebarElement = document.querySelector('.sidebar');
+const homeBtnElement = sidebarElement.querySelector('#home-btn');
+const articleFormOpenElement = sidebarElement.querySelector('#create-card');
+const articleFormCloseElement = document.querySelector('#close-btn');
+const notifnotificationsBtnElement = sidebarElement.querySelector('#notifications-btn');
+const messageBtnElement = sidebarElement.querySelector('#message-btn');
 
 const btnElements = [
-    homeBtn,
-    createBtn,
-    notifnotificationsBtn,
-    messageBtn,
-    articleFormCloseBtn
-]
+    homeBtnElement,
+    articleFormOpenElement,
+    notifnotificationsBtnElement,
+    messageBtnElement,
+    articleFormCloseElement
+];
 
 const openNavList = (evt) => {
-    for (let btn of btnElements) {
-        btn.classList.remove('open');
+    for (const btnElement of btnElements) {
+        btnElement.classList.remove('open');
     }
-    if (evt.target === articleFormCloseBtn) {
-        homeBtn.classList.add('open');
+
+    if (evt.target === articleFormCloseElement) {
+        homeBtnElement.classList.add('open');
     } else {
         evt.target.classList.add('open');
     }
 }
 
-for (let btnElement of btnElements) {
+for (const btnElement of btnElements) {
     btnElement.addEventListener('click', openNavList);
 }
