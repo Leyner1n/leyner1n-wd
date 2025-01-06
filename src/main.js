b29702c4e435d2a0bf81e7cece66c92831ca449f
Mikail Chughtai <mokhsinovich@mail.ru>
2025-01-07 02:00:22 +0300

    refactor

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import { generateArticles } from './data.js';
 import { renderArticleList } from './article-list.js';
 import { setFilterClickHandler } from './filters.js';
 import './article-form-modal.js';
-import './article-form-validation.js';
-import './animations/header-animations.js';
-import './animations/aside-animations.js';
+import './article-form.js';
+import './main-header.js';
+import './sidebar.js';
 
 const articleListElement = document.getElementById('app');
 const ARTICLE_COUNT = 30;
