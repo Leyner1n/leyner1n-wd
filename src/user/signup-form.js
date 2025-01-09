129145b797d71462f7f8193ea0a619fe92133884
Mikail Chughtai <mokhsinovich@mail.ru>
2025-01-09 21:40:29 +0300

    registration

---
+++ src/user/signup-form.js
@@ -0,0 +1,28 @@
+import { HTTPResponseStatus as Response } from '../enum.js';
+
+const signupFormElement = document.querySelector('#signup-modal form');
+
+const SIGNUP_URL = 'https://kekstagram.webdot.pro/api/v1/register';
+
+const setSignupFormSubmit = () => {
+    signupFormElement.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        const xhr = new XMLHttpRequest();
+
+        xhr.addEventListener('load', () => {
+            if (xhr.status === Response.CREATED || xhr.status === Response.UNAUTHORIZED) {
+                console.log(xhr.response);
+            }
+        });
+
+        xhr.open('POST', SIGNUP_URL);
+        xhr.setRequestHeader('Accept', 'application/json');
+
+        const data = new FormData(signupFormElement);
+        xhr.send(data);
+    });
+}
+
+export {
+    setSignupFormSubmit
+}
